a4e4ba3cbbcadecff1185ed91a2c36a420887a9f
Aarush Maddula <100654110+AarushMaddula@users.noreply.github.com>
2024-10-08 21:24:37 -0400

    add css to game

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 function createPlayer(marker) {
 
+    let wins = 0;
+
+    const updateWins = function() {
+        wins++;
+    }
+
+    const getWins = function() {
+        return wins;
+    }
+
+    const resetWins = function() {
+        wins = 0;
+    }
+
     const getMarker = function() {
         return marker;
     }
 
-    return {getMarker}
+    return {getMarker,updateWins, getWins, resetWins}
 }
 
 function createSquare() {
@@ -38,6 +52,14 @@ const GameBoard = (function() {
         gameBoard.push(row);
     }
 
+    const reset = function() {
+        gameBoard.forEach((row) => {
+            row.forEach((square) => {
+                square.setOccupant(" ");
+            })
+        })
+    }
+
     const getSquare = function(row, col) {
         return gameBoard[row][col];
     }
@@ -47,7 +69,7 @@ const GameBoard = (function() {
     }
 
     const isEmptySquare = function(row, col) {
-        return row < gameBoard.length && col < gameBoard[0].length;
+        return row < gameBoard.length && col < gameBoard[0].length && gameBoard[row][col].getSquareMarker() === " ";
     }
 
     const makeMove = function(row, col, player) {
@@ -64,7 +86,7 @@ const GameBoard = (function() {
         let returnValue = -1;
 
         gameBoard.forEach((rowArr, rowIndex) => {
-            if (isArraySame(rowArr)) returnValue = getSquare(rowIndex, 0);
+            if (isArraySame(rowArr)) returnValue = getSquare(rowIndex, 0).getOccupant();
         })
 
         if (returnValue !== -1) {
@@ -79,7 +101,7 @@ const GameBoard = (function() {
                 colArr.push(getSquare(row, col));
             }
 
-            if (isArraySame(colArr)) returnValue = getSquare(0, col);
+            if (isArraySame(colArr)) returnValue = getSquare(0, col).getOccupant();
         }
 
         if (returnValue !== -1) {
@@ -93,7 +115,7 @@ const GameBoard = (function() {
             seArr.push(getSquare(i, i))
         }
 
-        if (isArraySame(seArr)) return getSquare(0, 0);
+        if (isArraySame(seArr)) return getSquare(0, 0).getOccupant();
 
         const neArr = [];
 
@@ -101,40 +123,123 @@ const GameBoard = (function() {
             neArr.push(getSquare(gameBoard.length - 1 - i, i))
         }
 
-        if (isArraySame(neArr)) return getSquare(gameBoard.length - 1, 0);
+        if (isArraySame(neArr)) return getSquare(gameBoard.length - 1, 0).getOccupant();
     
-        return null;
+        if (gameBoard.every((row) => row.every((square) => square.getSquareMarker() !== " "))) return "tie";
+
+        return "continue";
     } 
 
-    const displayGameBoard = function() {
-        let text = "";
-
-        gameBoard.forEach((row, rowIndex) => {
-            row.forEach((col, colIndex) => {
-               text += ` ${col.getSquareMarker()} `
-               if (colIndex !== gameBoard[0].length - 1) text += "|";
-            })
-
-            if (rowIndex !== gameBoard.length - 1) text += "\n - | - | - \n";
-        })
-
-        console.log(text)
+    const getGameBoard = function() {
+        return gameBoard;
     }
 
-    return {isEmptySquare, makeMove, checkForWinner, displayGameBoard}
+    return {isEmptySquare, makeMove, checkForWinner, getGameBoard, reset}
 })()
 
-const gameController = (function() {
+const GameController = (function() {
+
+    const gameBoardContainer = document.querySelector(".game-board-container");
+
+    for (i = 0; i < 9; i++) {
+        const squareElement = document.createElement('div');
+        squareElement.classList.add("game-board-tile");
+        squareElement.setAttribute('data-index', i);
+        
+        squareElement.addEventListener('click', (event) => {
+            GameController.placeMarker(squareElement.getAttribute("data-index"));
+        })
+
+        gameBoardContainer.appendChild(squareElement);
+    }
+
+    const resetGame = document.querySelector('.reset-button');
+
+    resetGame.addEventListener('click', () => {
+        GameBoard.reset();
+        player1.resetWins();
+        player2.resetWins();
+        updateUserScores();
+        displayGameBoard();
+    })
+
     const player1 = createPlayer("X");
     const player2 = createPlayer("O");
 
-    GameBoard.makeMove(0, 0, player1)
-    GameBoard.makeMove(0, 1, player1)
-    GameBoard.makeMove(0, 2, player1)
+    let currentPlayer = player1;
 
+    const placeMarker = function (index) {
 
-    GameBoard.displayGameBoard();
-    const winner = GameBoard.checkForWinner();
-    if (winner !== null) console.log(`${winner.getSquareMarker()} won!`)
+        const row = Math.floor(index / 3);
+        const col = index % 3;
+    
+        if (!GameBoard.isEmptySquare(row, col)) return;
 
+        GameBoard.makeMove(row, col, currentPlayer);
+
+        displayGameBoard();
+
+        const winner = GameBoard.checkForWinner();
+
+        if (winner !== "continue") displayWinner(winner);
+
+        currentPlayer = currentPlayer === player1 ? player2 : player1; 
+    }
+
+    const displayWinner = function(player) {
+        const dialog = document.querySelector(".winner-dialog");
+        const dialogMessage = document.querySelector(".dialog-message");
+
+        if (Object.getPrototypeOf(player) === createPlayer) {
+            player.updateWins();
+        }
+
+        let text;
+        if (player === "tie") {
+            text = `Tie!`
+        } else {
+            text = `${player.getMarker()} won!`
+            player.updateWins();
+        }
+
+        updateUserScores();
+
+        dialogMessage.textContent = text;
+
+        const closeDialogButton = document.querySelector('.close-dialog-button');
+
+        closeDialogButton.addEventListener('click', (e) => {
+            GameBoard.reset();
+            displayGameBoard();
+
+            dialog.close();
+        })
+
+        dialog.showModal();
+    }
+
+    const updateUserScores = function() {
+        const player1Element = document.querySelector('.player-1 > .profile-score');
+        const player2Element = document.querySelector('.player-2 > .profile-score');
+
+        player1Element.textContent = `${player1.getWins()}`
+        player2Element.textContent = `${player2.getWins()}`
+    }
+
+    const displayGameBoard = function() {
+        const gameBoard = GameBoard.getGameBoard();
+
+        gameBoard.forEach((rowArr, rowIndex) => {
+            rowArr.forEach((square, colIndex) => {
+               const squareElement = document.querySelector(`.game-board-container > div[data-index="${rowIndex * 3 + colIndex}"]`);
+
+                squareElement.textContent = square.getSquareMarker();
+            })
+        })
+    }
+
+    
+
+    return {placeMarker, displayGameBoard, updateUserScores}
 }) ()
+
